dea82bd3460cd635ff55b0b844a46d3c936b24f6
zaorak <peter-hauvre@hotmail.com>
2026-01-13 13:45:51 +0100

    Update warmup.py

--- warmup.py
+++ warmup.py
@@ -1,8 +1,9 @@
 import os
 import requests
 
-APP_URL = os.environ["APP_URL"]
+APP_URL = os.environ["APP_URL"].rstrip("/")
+url = APP_URL + "/?warmup=1"
 
-r = requests.get(APP_URL, timeout=60)
+r = requests.get(url, timeout=180)
 print("warmup status:", r.status_code)
 r.raise_for_status()
